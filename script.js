cb5bfe99e2575a24bbdf550b2bb6edc0c356d111
Saulo Erick <sauloe@hotmail.com>
2023-04-27 11:34:52 -0400

    then, catch, finally

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- var consultaCEP = fetch('https://viacep.com.br/ws/0100100/json/')
+ var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
  .then(resposta => resposta.json())
  .then(r => {
     if (r.erro) {
@@ -6,6 +6,7 @@
         } else
             console.log(r)
     })
- .catch(erro => {console.log(erro)});
+ .catch(erro => console.log(erro))
+ .finally(_mensagem => console.log('Processamento concluído'))
 
  console.log(consultaCEP)
