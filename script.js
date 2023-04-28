ea6803fe057a3e0d232c023a222a6ca6066a0cd5
Saulo Erick <sauloe@hotmail.com>
2023-04-28 13:41:54 -0400

    Teste async

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-async function buscaEndereco(){
+async function buscaEndereco(cep){
     try {
-        var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/');
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvert = await consultaCEP.json();
         if (consultaCEPConvert.erro) {
             throw Error('Este CEP não Existe!');
         }
         console.log(consultaCEPConvert);
+        return consultaCEPConvert;
     } catch (erro) {
-        console.log(erro)
+        if (erro.name == 'TypeError'){
+            console.log('CEP inválido.\nCEP deve conter 8 dígitos!')
+        } else{
+            console.log(erro);
+        }
     }
 }
-
-buscaEndereco();
